903b4a0116678bc924e9e5251842b8bf06e34834
Raúl Pérez Vicente <rperezv@ayesa.com>
2024-12-07 20:10:31 +0100

    60. HTTP GET with Spring MVC List Operation

--- src/main/java/com/ayesa/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/ayesa/spring6restmvc/controller/BeerController.java
@@ -4,8 +4,10 @@ import com.ayesa.spring6restmvc.model.Beer;
 import com.ayesa.spring6restmvc.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 /**
  * BeerController
@@ -16,12 +18,20 @@ import java.util.UUID;
  * @version 07/12/2024 - 16:58
  * @since 1.17
  */
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class BeerController {
 
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beers")
+    public List<Beer> listBeers() {
+
+        log.info("BeerController.listBeers() (in controller) was called!");
+
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID beerId) {
 
--- src/main/java/com/ayesa/spring6restmvc/services/BeerService.java
+++ src/main/java/com/ayesa/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package com.ayesa.spring6restmvc.services;
 
 import com.ayesa.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID beerId);
 
